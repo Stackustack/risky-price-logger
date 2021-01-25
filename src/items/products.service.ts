234f3416abfa1866a01481107ea12a32cd50f15b
Michał Jung <stackustack@gmail.com>
2021-01-25 18:51:56 +0100

    Add method for getting and formatting product name

--- src/items/products.service.ts
+++ src/items/products.service.ts
@@ -4,16 +4,23 @@ import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductsService {
-    watchProduct(createProductDto: CreateProductDto) {
+    async watchProduct(createProductDto: CreateProductDto) {
         const { url } = createProductDto 
 
-        const price = this.fetchPrice(url)
-        // get readableName
-        // add product to DB
-        return price
+        const price = await this.fetchPrice(url)
+        const productName = this.getReadableName(url)
+
+        // TODO 1: add product to DB (if its not there yet)
+        // TODO 2: add priceLog for product
+
+        return ({
+            price,
+            productName
+        })
+
     }
 
-    private async fetchPrice(url) {
+    private async fetchPrice(url: string): Promise<number> {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
@@ -23,10 +30,20 @@ export class ProductsService {
             return document.querySelector(".ty-product-block__price-actual .ty-price-update .ty-price bdi .ty-price-num:first-of-type").textContent
         });
 
-        console.log(price)
-
         await browser.close();
 
         return price
+    }
+
+    private getReadableName(url: string): string {
+        if (url.endsWith('/')) {
+            url = url.slice(0, -1)
+        } 
+
+        const pathWithoutDashes = url.slice(url.lastIndexOf('/') + 1)
+            .replace(/-/g, ' ')
+        const productName = pathWithoutDashes.charAt(0).toUpperCase() + pathWithoutDashes.slice(1)
+
+        return productName
     }
 }
