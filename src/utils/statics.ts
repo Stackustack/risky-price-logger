634974b6b339bbfb6610628c3e44754803b42159
Michał Jung <stackustack@gmail.com>
2021-03-04 15:26:03 +0100

    Add statics helper for tests

---
+++ src/utils/statics.ts
@@ -0,0 +1,4 @@
+export const STATIC_DATA = {
+    productId: '1f6588bb-a9b5-4862-a5b6-e976934a77d9',
+    exampleShopUrl: 'https://www.shop-online.com/en/clothesw-category/dresses/little-chequered-dress-red/'
+}
