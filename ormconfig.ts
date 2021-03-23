b32863d4e5452cf5d88359fcaaea0f024b9e2345
Michał Jung <stackustack@gmail.com>
2021-03-23 13:22:39 +0100

    Workaround for SSL

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,20 +1,23 @@
 const {
-    DATABASE_HOST,
-    DATABASE_PORT,
-    DATABASE_USER,
-    DATABASE_PASS,
-    DATABASE_NAME,
-  } = process.env;
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASS,
+  DATABASE_NAME,
+} = process.env;
 
 const config = {
-    type: 'postgres',
-    host: DATABASE_HOST,
-    port: Number(DATABASE_PORT),
-    username: DATABASE_USER,
-    password: DATABASE_PASS,
-    database: DATABASE_NAME,
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true,
-  };
+  type: 'postgres',
+  host: DATABASE_HOST,
+  port: Number(DATABASE_PORT),
+  username: DATABASE_USER,
+  password: DATABASE_PASS,
+  database: DATABASE_NAME,
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false
+  },
+};
 
 module.exports = config;
